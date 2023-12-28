59254d25c13349dd3353b277ec6614df3b5c8887
John Canal <johncanal300@gmail.com>
2023-12-28 19:40:09 +0800

    Changed the web stand to twitter, made the tweet button, and did the tweet input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,51 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Tweet } from "@/components/Tweet";
-import { tweets } from "@/data/tweets";
+import { Tweet as TweetType } from "@/data/tweets";
+import { ChangeEvent, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [tweetText, setTweetText] = useState("");
+  const [tweets, setTweets] = useState<TweetType[]>([]);
+
+  const onTweetChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTweetText(e.target.value);
+  };
+
+  const onTweet = () => {
+    if (tweetText.length === 0) {
+      return;
+    }
+    const tweet: TweetType = {
+      id: tweets.length,
+      name: "Say2Cups",
+      username: "Say2Cups",
+      text: tweetText,
+      imageUrl:
+        "https://wallpapers.com/images/hd/family-guy-stewie-griffin-z8if6xxqftztkwrk.jpg",
+    };
+
+    setTweets([...tweets, tweet]);
+    setTweetText("");
+  };
+
   return (
     <main>
-      <h1 className="twitter-title">Web stand</h1>
+      <h1 className="twitter-title">Twitter</h1>
+
+      <div className="button-class">
+        <input
+          value={tweetText}
+          className="change-white"
+          onChange={onTweetChange}
+        />
+        <button className="button-blue" onClick={onTweet}>
+          Tweet
+        </button>
+      </div>
+
       {tweets.map((tweet) => {
         return <Tweet key={tweet.id} tweet={tweet} />;
       })}
